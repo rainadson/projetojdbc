8595df6c87bd3bea5deb20ff405da2335b1271e1
rainadson <rainadson@Rainadson>
2021-11-29 11:40:29 -0300

    Inclusão do INSERT

--- pos-java-jdbc/src/main/java/DAO/UserPosDAO.java
+++ pos-java-jdbc/src/main/java/DAO/UserPosDAO.java
@@ -1,8 +1,10 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import conexaojdbc.SingleConnection;
+import model.Userposjava;
 
 public class UserPosDAO {
 	
@@ -11,5 +13,29 @@ public class UserPosDAO {
 	public UserPosDAO() {
 		connection  = SingleConnection.getConnection(); 
 	}
+	
+	public void salvar (Userposjava userposjava) {
+		try {
+			String sql = "insert into userposjava (id, nome, email) values(?,?,?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+			insert.setLong(1, userposjava.getId());
+			insert.setString(2,userposjava.getNome());
+			insert.setString(3,userposjava.getEmail());	
+			insert.execute();
+			connection.commit();/*salva no banco de dados*/
+			
+		} catch (Exception e) {
+			try {
+				connection.rollback();	// reverte operação	
+			} catch (Exception e2) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
 
 }
--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,13 +2,28 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.jupiter.api.Test;
 
-import conexaojdbc.SingleConnection;
+import DAO.UserPosDAO;
+import model.Userposjava;
 
-public class TesteBancoJdbc {
+public class TesteBancoJdbc{
 	
 	@Test
-	public void initBanco() {
-		SingleConnection.getConnection();
+	public void initBanco(){
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(5);
+		userposjava.setNome("paulo");
+		userposjava.setEmail("pauloampaios@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
+		
+		
 	}
+	/*	UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userPosDAO.salvar(userposjava);
+	}*/
 
 }
